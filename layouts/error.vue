b9fe9ca5146a733a9948f91c411bfa37184c85af
Alexis <35.alexis.pele@gmail.com>
2021-03-29 16:18:37 +0200

    + Added layout page and error handler

---
+++ layouts/error.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="error bg-image">
+    <div class="splash">
+      <h2 class="heading-1">
+        <span v-if="error.statusCode === 404">Page non trouvée</span>
+        <span v-else>Une erreur est survenue</span>
+        <span>:(</span>
+      </h2>
+      <p>{{ error.message }}</p>
+      <div>
+        <nuxt-link :to="'/'">
+          <AtomsFSButton>Retourner à la page d'accueil</AtomsFSButton>
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: {
+    error: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.error {
+  position: relative;
+  min-height: inherit;
+  padding: 0 10%;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+
+  &:after {
+    background-image: url("/celestials_bg-min.jpg");
+  }
+}
+</style>
